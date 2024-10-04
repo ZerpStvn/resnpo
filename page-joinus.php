a7e0001f5834ad2b80b77a9af06db6aa33f8d0b3
adrian-gfw <adrianabayon.gfw@gmail.com>
2024-10-04 08:38:49 +0800

    Started creating joinus-page

---
+++ page-joinus.php
@@ -0,0 +1,57 @@
+<?php get_header() ?>
+<div id="joinusid">
+
+<section class="section-project-header relative">
+    <div class="project-header-content relative">
+      <p class="header-text" >JOIN <br> US</p>
+    </div>
+    <img class="absolute img-people" src="<?php echo RESNPO_URI . '/assets/image/project/header/people-img.png' ?>" alt="">
+  
+    <div class="header-text-items relative">
+      <p class="header-item-text">Be part of our journey!</p>
+      <ul class="ul-text-items">
+        <li>
+          <div class="header-text-item relative">
+            <p>キフ</p>
+            <p>Sed ut perspiciatis unde 
+            omnis iste natus error sit 
+            voluptatem accusantium </p>
+            <img class="absolute front-img" src="<?php echo RESNPO_URI . '/assets/image/project/header/ellipse-1.png' ?>" alt="">
+            <img class="absolute back-img" src="<?php echo RESNPO_URI . '/assets/image/project/header/ellipse-bg.png' ?>" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="header-text-item relative">
+            <p>メンバーシップ</p>
+            <p>Sed ut perspiciatis unde 
+            omnis iste natus error sit 
+            voluptatem accusantium </p>
+            <img class="absolute front-img" src="<?php echo RESNPO_URI . '/assets/image/project/header/ellipse-2.png' ?>" alt="">
+            <img class="absolute back-img" src="<?php echo RESNPO_URI . '/assets/image/project/header/ellipse-bg.png' ?>" alt="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <div class="marquee-container">
+    <div class="marquee">
+      <ul class="flex space-between">
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>実学</p></li>
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>文化交流</p></li>
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>海外支援</p></li>
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>実学</p></li>
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>文化交流</p></li>
+        <li><img src="<?php echo RESNPO_URI . '/assets/image/project/leaves.png' ?>" alt=""></li>
+        <li><p>海外支援</p></li>
+      </ul>
+    </div>
+  </div>
+
+</div>
+<?php get_footer() ?>
