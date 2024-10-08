96ef51b8d62c706dff6bf29abb0dc6717d4df78e
GFW Jim <jimuel.gfw@gmail.com>
2024-10-08 15:13:58 +0800

    new branch

--- page-about.php
+++ page-about.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
-<div class="aboutid">
+<div id="aboutid" class="aboutid">
 
   <section class="landing-about">
     <header>
@@ -7,26 +7,170 @@
     </header>
 
 
-    <div class="social">
-      <img src="<?php echo RESNPO_IMAGE . '/line.png' ?>" alt="">
-      <img src="<?php echo RESNPO_IMAGE . '/facebook.png' ?>" alt="">
-      <img src="<?php echo RESNPO_IMAGE . '/youtube.png' ?>" alt="">
-    </div>
-
-    <div class="sdg-title">
-      <div class="side-title">
-        <img class="side-logo" src="<?php echo RESNPO_IMAGE . '/logo.png' ?>" alt="">
-        <div class="side-title-text">
-          <h1>NPO法人</h1>
-          <h3>REGIONAL EDUCATION SUPPORT</h3>
+    <div class="side-content">
+      <div class="social">
+        <img src="<?php echo RESNPO_IMAGE . '/line.png' ?>" alt="">
+        <img src="<?php echo RESNPO_IMAGE . '/facebook.png' ?>" alt="">
+        <img src="<?php echo RESNPO_IMAGE . '/youtube.png' ?>" alt="">
+      </div>
+      <div class="about-title">
+        <div class="side-title">
+          <img class="side-logo" src="<?php echo RESNPO_IMAGE . '/logo.png' ?>" alt="">
+          <div class="side-title-text">
+            <h1>NPO法人</h1>
+            <h3>REGIONAL EDUCATION SUPPORT</h3>
+          </div>
         </div>
       </div>
+    </div>
 
-      <div class="au-title">
-        <h1 class="outline">About Us</h1>
+    <div class="au-title">
+      <h1 class="outline au-main-title">ABOUT US</h1>
+      <h3 class="au-sub-title sub-head-clr">What is RES?</h3>
+    </div>
+
+  </section>
+
+
+  <div class="vma-title">
+    <h1>ひとりでも多くの子どもたちが、<br>未来の可能性を芸術で表現できる社会を目指して</h1>
+  </div>
+
+  <section id="vision-section" class="diagonal-section">
+    <div class="rotated-text">VISION</div>
+    <div class="section-content head-clr flex-rr head-clr">
+      <div class="image-placeholder">
+        <img src=" <?php echo RESNPO_IMAGE . '/about/vision1.png' ?>" alt="">
       </div>
+      <div class="section-text">
+        <h2 class="section-title">実現したい社会</h2>
+        <p>
+          人の役に立ちたい！人に必要とされたい！ そんな想いをきっかけに、ご縁があって2003年にこのNPO法人を立ち上げました。そして、フィリピンへ幼稚園の寄付、ボランティア活動などを行っていく中で湧いてきた新たな想い。
+
+          <br><br>
+
+          それは、「<span class="white-clr">このままではまずい…</span>」。 
+
+          <br>
+
+          海外の若い人たちは、英語をコミュニケーションのツールとして使いこなし、国境や人種の壁を越えて活躍しています。その圧倒的な熱量と活発に動く姿をみていると、このままでは<span class="white-clr">日本は世界から取り残されてしまう</span>のではないか？そんな危機感を強く感じました
+
+          <br><br>
+
+          そんな時に現地で出会ったのがJICA（ジャイカ：青年海外協力隊）のメンバーとして活躍していた人たち、また、熱い想いを持って海外で活躍している人たち。
+
+          <br><br>
+
+          彼ら彼女らと交流することで、「日本人が活躍している社会」への希望を感じることができました。
+
+          <br><br>
+          今、私たちだけでできることには限りがあるかも知れない。だけど、無限の可能性を持つ子どもたちに、この先待ち受けるグローバル社会について一つでも多く伝えたい。また、その時代を生き抜く力を身につける、養うためのサポートをしたい。そう強く思っています。
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section id="mission-section" class="diagonal-section">
+    <div class="rotated-text">MISSION</div>
+    <div class="section-content head-clr flex-row">
+      <div class="image-placeholder">
+        <img src="<?php echo RESNPO_IMAGE . '/about/mission1.png' ?>" alt="">
+      </div>
+      <div class="section-text">
+        <h2 class="section-title">私たちの使命</h2>
+        <p>
+          あたり前のことをあたり前だからと思考停止するのではなく、なぜなんだろう？と考え、自分なりの答えを持つ。 間違っていてもいい
+
+          <br><br>
+
+          間違いに気づけば考えを改めればいい。そして、考えて終わりではなく、行動する。
+          <br><br>
+
+          私たちは3つのプロジェクトを通して、子供たちに自ら考えて行動することの大切さを伝えています。
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section id="action-section" class="diagonal-section">
+    <div class="rotated-text">ACTION</div>
+    <div class="section-content head-clr head-clr flex-rr">
+      <div class="image-placeholder">
+        <img src="<?php echo RESNPO_IMAGE . '/about/action1.png' ?>" alt="">
+      </div>
+      <div class="section-text">
+        <h2 class="section-title">私たちの行動</h2>
+        <p>
+          あたり前のことをあたり前だからと思考停止するのではなく、なぜなんだろう？と考え、自分なりの答えを持つ。
+          <br><br>
+           間違っていてもいい
+          <br><br>
+          間違いに気づけば考えを改めればいい。そして、考えて終わりではなく、行動する。
+          <br><br>
+          私たちは3つのプロジェクトを通して、子供たちに自ら考えて行動することの大切さを伝えています。
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section id="realxlink">
+    <div class="rxl-ur">
+      <h1 class="qt flex">“</h1>
+      <div class="content flex flex-row">
+        <p>
+          NPO法人 RESは、ひとりでも多くの子どもたちが、
+          未来の国際社会で活躍できる社会を目指し、
+          自ら考え行動できる人材を育成する活動をしています。
+        </p>
+        <div class="image-container">
+          <img src="<?php echo RESNPO_IMAGE . '/about/message.png' ?>" alt="">
+          <div class="rotated-text">MESSAGE</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="rxl-lr">
+      <img src="<?php echo RESNPO_IMAGE . '/about/mori.jpeg' ?>" alt="">
+      <div class="rxl-lr-txt">
+        <h1 class="sb_title text-centered">新時代の学びのカタチ「REAL✕LINK」</h1>
+        <p>
+          新しい時代「令和」が始まりました。 新しい元号のもと、日本にとどまらず世界中から争いごとや飢えや難病が無くなっていくことを心から願っています。  さて、いつの時代においても最も重要な事業のひとつに位置づけられるのが「教育」です。 先進国も発展途上国も、大きな国も小さな地域も、都会でも田舎でも、「教育」はとても大切です。 
+          <br><br>
+
+          しかし現状では、将来を担う子供たちに人間形成の土台となる基礎知識や手法を伝え、 さらに創造性や思考力を育むのは「公的機関」だけでは不充分だと思います。
+          <br><br>
+
+            私たちは、身近な地域の子供たちに何かの手助けができないか、「民間」の利を活かした教育ができないかと考えました。
+          <br><br>
+
+            ますますグローバル化するこのREALな世界で生きる子供たちが、年齢や地域を越えてLINKし、いろんな人と繋がることで、 お互いを知り、「学ぶ心」や「人を想いやる心」を醸成し、共に考え共に成長する。 そんな新しい時代の学びの場（カタチ）をみんなで創って行きたいと思っています。 そして、その事を通して、一人でも多くの「自ら考え行動できる人」が育ってくれれば幸いです。
+
+        </p>
+      </div>
+    </div>
+
+
   </section>
 
 
 </div>
+
+
+<svg version="1.1" xmlns="//www.w3.org/2000/svg" xmlns:xlink="//www.w3.org/1999/xlink" style="display:none;">
+  <defs>
+    <filter id="stroke-text-svg-filter">
+      <feMorphology operator="dilate" radius="4"></feMorphology>
+      <feComposite operator="xor" in="SourceGraphic" />
+    </filter>
+  </defs>
+</svg>
+
+<svg version="1.1" xmlns="//www.w3.org/2000/svg" xmlns:xlink="//www.w3.org/1999/xlink" style="display:none;">
+  <defs>
+    <filter id="stroke-text-svg-filter">
+      <feMorphology operator="dilate" radius="4"></feMorphology>
+      <feComposite operator="xor" in="SourceGraphic" />
+    </filter>
+  </defs>
+</svg>
 <?php get_footer() ?>
