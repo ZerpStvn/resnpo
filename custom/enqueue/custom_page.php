105de0464853edf927749e04fc18cbecf74aec38
GFW Jim <jimuel.gfw@gmail.com>
2024-10-09 12:55:45 +0800

    fix

--- custom/enqueue/custom_page.php
+++ custom/enqueue/custom_page.php
@@ -2,98 +2,99 @@
 
 function add_custom_page()
 {
-    $pages = array(
-        array(
-            'post_title' => 'Home',
-            'post_content' => 'Home',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'home'
-        ),
-        array(
-            'post_title' => 'About',
-            'post_content' => 'About',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'about'
-        ),
-        array(
-            'post_title' => 'Project',
-            'post_content' => 'project',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'project'
-        ),
-        array(
-            'post_title' => 'SDG',
-            'post_content' => 'SDG',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'sdg'
-        ),
-        array(
-            'post_title' => 'Join Us',
-            'post_content' => 'joinus',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'joinus'
-        ),
-        array(
-            'post_title' => 'Donation',
-            'post_content' => 'donation',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'donation'
-        ),
-        array(
-            'post_title' => 'Membership',
-            'post_content' => 'membership',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'membership'
-        ),
-        array(
-            'post_title' => 'What\'s New',
-            'post_content' => 'wahtsnew',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'whatsnew'
-        ),
-        array(
-            'post_title' => 'Study Abroad',
-            'post_content' => 'Study Abroad',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'study-abroad'
-        ),
-        array(
-            'post_title' => 'Studying Abroad',
-            'post_content' => 'Studying Abroad',
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'page',
-            'post_name' => 'studying-abroad'
-        ),
-    );
+  $pages = array(
+    array(
+      'post_title' => 'Home',
+      'post_content' => 'Home',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'home'
+    ),
+    array(
+      'post_title' => 'About',
+      'post_content' => 'About',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'about'
+    ),
+    array(
+      'post_title' => 'Project',
+      'post_content' => 'project',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'project'
+    ),
+    array(
+      'post_title' => 'SDG',
+      'post_content' => 'SDG',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'sdg'
+    ),
+    array(
+      'post_title' => 'Join Us',
+      'post_content' => 'joinus',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'joinus'
+    ),
+    array(
+      'post_title' => 'Donation',
+      'post_content' => 'donation',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'donation'
+    ),
+    array(
+      'post_title' => 'Membership',
+      'post_content' => 'membership',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'membership'
+    ),
+    array(
+      'post_title' => 'What\'s New',
+      'post_content' => 'wahtsnew',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'whatsnew'
+    ),
+    array(
+      'post_title' => 'Study Abroad',
+      'post_content' => 'Study Abroad',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'study-abroad'
+    ),
+    array(
+      'post_title' => 'Studying Abroad',
+      'post_content' => 'Studying Abroad',
+      'post_status' => 'publish',
+      'post_author' => 1,
+      'post_type' => 'page',
+      'post_name' => 'studying-abroad'
+    ),
+  );
 
-    foreach ($pages as $page) {
-        $existing_page = get_page_by_path($page['post_name']);
-        if (is_null($existing_page)) {
-            wp_insert_post($page);
-        }
+  foreach ($pages as $page) {
+    $existing_page = get_page_by_path($page['post_name']);
+    if (is_null($existing_page)) {
+      wp_insert_post($page);
     }
   }
   $homepage = get_page_by_path('home');
   update_option('page_on_front', $homepage->ID);
   update_option('show_on_front', 'page');
 }
+
+
 add_action('init', 'add_custom_page');
