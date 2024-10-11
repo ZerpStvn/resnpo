95eaa30885feac019217c1ff7c849e52db4a8945
GFW Jim <jimuel.gfw@gmail.com>
2024-10-11 11:05:47 +0800

    added dynamic news

--- custom/main.php
+++ custom/main.php
@@ -11,3 +11,4 @@ require_once(custom_dir . "/front/sgd-landing.php");
 require_once(custom_dir . "/front/feature-posttype.php");
 require_once(custom_dir . "/front/photo-gallery-posttype.php");
 require_once(custom_dir . "/front/past-performance-posttype.php");
+require_once(custom_dir . "/front/news-posttype.php");
