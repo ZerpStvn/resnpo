bf450823adc34c133232caaf11274c3a14ee15f3
zerpsteve <stevefelizardo4@gmail.com>
2024-10-22 11:41:45 +0800

    update

--- custom/main.php
+++ custom/main.php
@@ -2,7 +2,7 @@
 define("custom_dir", RESNPO_DIR . "/custom");
 require_once(custom_dir . "/enqueue/custom_page.php");
 require_once(custom_dir . "/front/custom-fronts.php");
-
+require_once(custom_dir . "/front/event.php");
 require_once(custom_dir . "/front/feature-posttype.php");
 require_once(custom_dir . "/front/photo-gallery-posttype.php");
 require_once(custom_dir . "/front/past-performance-posttype.php");
--- page-home.php
+++ page-home.php
@@ -6,7 +6,7 @@
 
       <img class="desktop_logo" src="<?php echo RESNPO_URI . '/assets/image/homepage/header/top_nav_txt.png' ?>"
         alt="logo" loading="lazy">
-      <img class="mobile_logo" src="<?php echo RESNPO_URI . '/assets/image/homepage/header/mobilelogo.png' ?>"
+      <img class="mobile_logo" src="<?php echo RESNPO_URI . '/assets/image/homepage/header/resnewlogo.svg' ?>"
         alt="logo" loading="lazy">
       <hr>
       <div>
@@ -181,6 +181,14 @@
   <!-- END SECTION -->
 
   <section class="section_3 relative global-width">
+    <div class="contenttitletop supporter">
+      <div class="contenttoptitlewrap">
+
+        <img src="<?php echo RESNPO_URI . '/assets/image/homepage/side-section-img.png' ?>" alt="" />
+        <p>NEWS</p>
+
+      </div>
+    </div>
     <ul class="flex ul-news">
       <?php
       $whatsnew_query = new WP_Query([
@@ -192,7 +200,7 @@
         while ($whatsnew_query->have_posts()):
           $whatsnew_query->the_post();
           $whatsnew_date = get_post_meta(get_the_ID(), '_whatsnew_date', true);
-      ?>
+          ?>
           <li>
             <a href="<?php echo get_permalink() ?>" id="main-article">
               <div class="img-container-news">
@@ -212,7 +220,7 @@
               </div>
             </a>
           </li>
-      <?php
+          <?php
         endwhile;
         wp_reset_postdata();
       endif;
@@ -231,7 +239,7 @@
             while ($recent_whatsnew_query->have_posts()):
               $recent_whatsnew_query->the_post();
               $recent_whatsnew_date = get_post_meta(get_the_ID(), '_whatsnew_date', true);
-          ?>
+              ?>
               <li>
                 <a href="<?php echo get_permalink() ?>">
                   <div class="flex with-img">
@@ -245,7 +253,7 @@
                   </div>
                 </a>
               </li>
-          <?php
+              <?php
             endwhile;
             wp_reset_postdata();
           endif;
@@ -263,7 +271,7 @@
   <!-- SECTION: NEWS/RESPRESENTATIVE -->
 
   <!-- <a href="<?php //echo get_site_url() . '/about#realxlink' 
-                ?>"> -->
+  ?>"> -->
   <section class="section-representative">
     <div class="top-title-news relative">
       <p class="p-230 w-700 slideright">NEWS</p>
@@ -309,7 +317,7 @@
 
   <!-- SECTION: ADVISOR -->
   <!-- <a href="<?php // echo RESNPO_URI . '/about#advisor_section' 
-                ?>"> -->
+  ?>"> -->
   <section id="advisor_home" class="flex flex-column relative">
     <div class="contenttitletop advisor">
       <div class="contenttoptitlewrap">
